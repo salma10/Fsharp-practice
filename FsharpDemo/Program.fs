0bd9ace8c9b1b059c68fff3c548ec6cec211f05e
salma10 <salma.cse10@gmail.com>
2021-02-06 01:09:02 +0600

    First commit Fsharp practice

---
+++ FsharpDemo/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let cond_stuff() = 
+    let age = 8
+
+    if age < 5 then
+        printfn "Pre "
+    elif age = 5 then
+        printfn "Kn"
+    elif (age > 5) && (age <= 18) then
+        let grade = age - 5
+        printfn "Go to dam %i" grade
+    else
+        printfn "Go to dam"
+   
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    cond_stuff()
+    0 // return an integer exit code
+
+
+
