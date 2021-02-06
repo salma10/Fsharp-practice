ab30c98ce4997b19edef31811ca1571db3a10081
salma10 <salma.cse10@gmail.com>
2021-02-06 17:32:13 +0600

    generic practice

--- FsharpDemo/Program.fs
+++ FsharpDemo/Program.fs
@@ -2,34 +2,18 @@
 
 open System
 
-let map_stuff() = 
-    let customers = 
-        Map.empty.
-            Add("Bob", 100.23).
-            Add("Bob2", 10.23)
+let add<'T> x y =
+    printfn "%A" (x + y)
 
-    printfn "Customers %i" customers.Count
-
-    let cust = customers.TryFind "Bob"
-    match cust with
-    | Some x -> printf "Balance : %.2f" x
-    | None -> printfn "Not found"
-
-    printfn "Customers : %A" customers
-
-    printfn "Bobs balance : %.2f" customers.["Bob"]
-
-    if customers.ContainsKey "Bob" then
-        printfn "Bob found"
-    
-    let cust2 = Map.remove "Bob" customers
-
-    printfn "Count : %i" cust2.Count
+let generic_stuff() = 
+    //add<float> 5.5 4.2
+    //add<int> 2 3
+    add<string> "x" "y"
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    map_stuff()
+    generic_stuff()
     0 // return an integer exit code
 
 
