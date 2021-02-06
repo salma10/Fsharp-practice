2844d9957a99ab07287adca0e4e9e65e8e826fc0
salma10 <salma.cse10@gmail.com>
2021-02-06 16:42:49 +0600

    record practice

--- FsharpDemo/Program.fs
+++ FsharpDemo/Program.fs
@@ -1,24 +1,18 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-
-let tuple_stuff() = 
-   let avg(x, y, z) : float = 
-    let sum = x + y + z
-    sum / 4.0
-   
-   printfn "Avg : %f" (avg (1.0, 2.0, 3.0))
-
-   let my_data = ("derek", 42, 6.24)
-
-   let (name, age, _) = my_data
-
-   printfn "Name : %s" name
+type customer =
+    { Name : string;
+      Balance : float
+    }
+let record_stuff() = 
+    let bob = { Name = "Bob naz"; Balance = 102.23}
+    printfn "%s owes us %.2f" bob.Name bob.Balance
    
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    tuple_stuff()
+    record_stuff()
     0 // return an integer exit code
 
 
