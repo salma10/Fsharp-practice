327b74c9037171cc7c48afe76081538279389ccd
salma10 <salma.cse10@gmail.com>
2021-02-06 17:44:45 +0600

    struct practice

--- FsharpDemo/Program.fs
+++ FsharpDemo/Program.fs
@@ -1,19 +1,27 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+type Rectangle = struct
+    val Length : float
+    val Width : float
 
-let add<'T> x y =
-    printfn "%A" (x + y)
+    new (length, width) =
+        {Length = length; Width = width}
+end
 
-let generic_stuff() = 
-    //add<float> 5.5 4.2
-    //add<int> 2 3
-    add<string> "x" "y"
+let exp_stuff() = 
+    let area( shape : Rectangle) =
+        shape.Width * shape.Length
+
+    let react = new Rectangle(5.0, 6.0)
+
+    let rect_area = area react
+    printfn "Area : %.2f" rect_area
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    generic_stuff()
+    exp_stuff()
     0 // return an integer exit code
 
 
