5290b0a8d6f6fecef3b51a4ef7906b8103d7144c
salma10 <salma.cse10@gmail.com>
2021-02-06 16:55:04 +0600

    Sequence practice

--- FsharpDemo/Program.fs
+++ FsharpDemo/Program.fs
@@ -1,18 +1,30 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-type customer =
-    { Name : string;
-      Balance : float
-    }
-let record_stuff() = 
-    let bob = { Name = "Bob naz"; Balance = 102.23}
-    printfn "%s owes us %.2f" bob.Name bob.Balance
+
+let seq_stuff() = 
+    let seq1 = seq { 1 .. 100 }
+    let seq2 = seq { 0 .. 2 .. 100 }
+    let seq3 = seq { 50 .. 1}
+
+    printfn "%A" seq1
+    printfn "%A" seq2
+    printfn "%A" seq3
+
+    Seq.toList seq2|> List.iter (printfn "Num : %i")
+
+    let is_prime n =
+        let rec check i =
+            i > n/2 || (n % i <> 0 && check (i + 1))
+        check 2
+
+    let prime_seq = seq { for n in 1..500 do if is_prime n then yield n}
+    printfn "%A" prime_seq
    
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    record_stuff()
+    seq_stuff()
     0 // return an integer exit code
 
 
