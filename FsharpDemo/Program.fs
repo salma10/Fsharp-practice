dde0fbdb165845869daf03607af12daf3819c62d
salma10 <salma.cse10@gmail.com>
2021-02-06 16:01:26 +0600

    more List practice

--- FsharpDemo/Program.fs
+++ FsharpDemo/Program.fs
@@ -16,6 +16,18 @@ let list_stuff() =
     let list4 = ['a'..'g']
     printfn "%A" list3
     printfn "%A" list4
+
+    let list5 = List.init 5 (fun i -> i * 2)
+    printfn "%A" list5
+
+    let list6 = [for a in 1..5 do yield( a * a)]
+    printfn "%A" list6
+
+    let list7 = [for a in 1..5 do if a % 2 = 0 then yield a]
+    printfn "%A" list7
+
+    let list8 = [for a in 1..5 do yield! [a..a+2]]
+    printfn "%A" list8
    
 [<EntryPoint>]
 let main argv =
